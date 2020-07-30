c3a87a09e9b8cd35cd027766226b738477eec909
Caroline <caroline_mendez@baylor.edu>
2020-07-30 12:10:08 -0500

    Motivational Quotes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quotes/ui/common/quote_widget.dart';
+import 'package:quotes/ui/pages/quote_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Quotes',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: QuoteWidget(
-          quote: 'Hello',
-          author: 'Me',
-          backgroundColor: Colors.redAccent,
-        ),
+        body: QuoteScreen(),
       ),
     );
   }
--- lib/ui/common/quote_widget.dart
+++ lib/ui/common/quote_widget.dart
@@ -1,29 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/screenutil.dart';
+import 'package:quotes/ui/styleguide/theme_text.dart';
 
 class QuoteWidget extends StatelessWidget {
   final Color backgroundColor;
   final String quote, author;
 
-  const QuoteWidget({Key key, this.backgroundColor, this.quote, this.author})
+  const QuoteWidget(
+      {Key key,
+      @required this.backgroundColor,
+      @required this.quote,
+      @required this.author})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, width: 350, height: 660);
-
     return Container(
+        margin: EdgeInsets.only(top: 50, bottom: 50, left: 15, right: 15),
+        width: ScreenUtil.screenHeightDp,
+        padding: EdgeInsets.symmetric(
+          horizontal: ScreenUtil().setWidth(16),
+          vertical: ScreenUtil().setHeight(48),
+        ),
         color: backgroundColor,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Image.asset(
               'assets/quote_img.png',
+              color: Colors.white.withOpacity(0.4),
               width: ScreenUtil().setWidth(70),
               height: ScreenUtil().setHeight(70),
             ),
-            Text(
-              quote, style: ,
-            )
+            Expanded(
+              child: Center(
+                child: Text(
+                  quote,
+                  style: ThemeText.headline,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                author,
+                style: ThemeText.subHead,
+              ),
+            ),
           ],
         ));
   }
